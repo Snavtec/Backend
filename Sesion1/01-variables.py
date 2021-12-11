77ee68d8e6709832da807acbb454b02ecf24b3c6
Snavtec <samuel.navarro2305@gmail.com>
2021-12-10 20:21:00 -0500

    chore: declaracion de variables

---
+++ Sesion1/01-variables.py
@@ -0,0 +1,46 @@
+# variable numerica
+numero = 10
+numero2 = 10.5
+
+numero = "Febrero"
+
+# variables de texto o string
+nombre = "Eduardo"
+apellido = 'Ramiro'
+
+html = '''<html>
+<p>
+</p>'''
+
+html = """<html>
+<p>
+</p>"""
+
+print('holaaaaaaaaa :3')
+
+print(type(nombre))
+#str = string
+#int = integer
+#float = float
+print(type(numero))
+print(type(numero2))
+#bool = boolean
+
+soltero = True
+calor = False
+print(type(soltero))
+
+# VARIABLES que tienes varios valores
+# ARREGLOS = LISTAS LIST
+edades = [10, 12, 40, 60, 'Eduardo', 14.5, False, {1, 2}]
+
+# JSON (JavaScript Object Notation) | Diccionario
+# Nota: Si una llave se repite su valor sera modificado y se perdera el anterior valor 
+curso = {
+    'nombre' : 'Backend',
+    'nombre' : 'Frontend',
+    'dificultad' : 'Dificil',
+}
+
+print(curso['dificultad'])
+fecha = 0
