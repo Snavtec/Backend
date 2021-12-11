ba5fdb10c3589d30590890758063fddd0d5121c9
Snavtec <samuel.navarro2305@gmail.com>
2021-12-10 21:59:18 -0500

    Otro commit :3

--- Sesion1/01-variables.py
+++ Sesion1/01-variables.py
@@ -33,14 +33,69 @@ print(type(soltero))
 # VARIABLES que tienes varios valores
 # ARREGLOS = LISTAS LIST
 edades = [10, 12, 40, 60, 'Eduardo', 14.5, False, {1, 2}]
+# para ingresar a los valores de una lista debemos indicar la POSICION que siempre empienza en 0 (CERO), ademas si queremos usar valores negativos entonces la lista empezara por la ultima posicion (-1 : la ultima posicion)
+# si nosotros en la posicion colocamos el siguiente formato: [n:m], entonces estaremos indicando que queremos ir desde la posicion 'n', hasta NOTA: siempre el recorrido sera de izquierda a derecha aun asi usemos posiciones negativas
+print(edades[2:4])
 
 # JSON (JavaScript Object Notation) | Diccionario
 # Nota: Si una llave se repite su valor sera modificado y se perdera el anterior valor 
 curso = {
-    'nombre' : 'Backend',
-    'nombre' : 'Frontend',
+    'nombre'     : 'Backend',
     'dificultad' : 'Dificil',
+    'skills'     : [     
+        {   
+            'nombre' : 'Base de datos',
+            'nivel'  : 'Intermedio'
+        },
+        {
+            'nombre' : 'ORM',
+            'nivel'  : 'Avanzado'
+        }
+    ]
 }
 
 print(curso['dificultad'])
-fecha = 0
+
+# quiero el nivel del skill ORM
+print(curso['skills'][1]['nivel'])
+
+[PrimerMes, segundo_mes, tercer_mes, cuarto_mes, quinto_mes] = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+
+print(PrimerMes)
+
+personas = [{   
+    'nombre'       : 'Eduardo',
+    'nacionalidad' : 'peruano',
+    'hobbies'       : [
+        {
+            'nombre'      : 'Volar drones',
+            'experiencia' : '2 años'
+        },{
+            'nombre'       : 'Juliana',
+            'nacionalidad' : 'colombiana',
+            'hobbies'      : [
+                {
+                    'nombre'      : 'Montar bici',
+                    'experiencia' : '4 años'
+                },{
+                    'nombre' : 'Bases de datos',
+                    'experiencia' : '8 meses'
+                }
+            ]
+        }
+    ]
+}]
+
+# nacionalidad de la primera persona
+
+print(personas[0]['nacionalidad'])
+
+# los hobbies de la segunda persona
+
+print(personas[1]['hobbies'])
+
+# la experiencia del segundo hobbie de la primera persona
+
+print(personas[0]['hobbies'][1]['experiencia'])
+
+
