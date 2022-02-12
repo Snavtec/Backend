044b8d8b29cf41b2963fa97c5bf978162d461c5e
Snavtec <samuel.navarro2305@gmail.com>
2022-02-12 17:53:07 -0500

    chore: avance recovery password

--- src/controllers/imagen.controller.js
+++ src/controllers/imagen.controller.js
@@ -1,12 +1,16 @@
-export function subirImagen(req, res) {
-    console.log(req.file); // aca se almacenara la imagen subida previamente al servidor gracias 
-    const { id } = req.params;
-    try {  
-        const respuesta = await ImagenService.subirImagen(id, req.file.path)
-        return res.status(200).json(respuesta);
-} catch (error) {
+import { ImagenService } from "../services/imagen.service.js";
+
+export async function subirImagen(req, res) {
+  console.log(req.file); // aca se almacera la imagen subida previamente al servidor gracias a multer
+  // si no instalamos multer entonces el req.file sera undefined ya que express no lee por defecto los archivos
+  const { id } = req.params;
+  try {
+    const respuesta = await ImagenService.subirImagen(id, req.file.path);
+
+    return res.status(200).json(respuesta);
+  } catch (error) {
     return res
-        .status(400)
-        .json({ message: "Error al subir la imagen", content: error.message });
-    }     
+      .status(400)
+      .json({ message: "Error al subir la imagen", content: error.message });
+  }
 }
--- src/routes/categorias.routes.js
+++ src/routes/categorias.routes.js
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import { 
-    crearCategoria,
-    devolverCategorias,
+import {
+  crearCategoria,
+  devolverCategorias,
+  eliminarCategoria,
 } from "../controllers/categorias.controller.js";
 
 export const categoriaRouter = Router();
 
-categoriaRouter
-    .route("/categoria")
-    .post(crearCategoria)
-    .get(devolverCategorias);
+categoriaRouter.route("/categoria").post(crearCategoria);
 
-// categoriaRouter.route("/categoria").post(crearCategoria);
-// categoriaRouter.get("/categoria", devolverCategorias);
+categoriaRouter.get("/categorias", devolverCategorias);
+
+categoriaRouter.route("/categoria/:id").delete(eliminarCategoria);
