0a2f0e926d76a0e29d6807fa8e5f92d95f0e2fd0
Snavtec <samuel.navarro2305@gmail.com>
2022-02-11 19:30:23 -0500

    chore: avance mongodb

--- src/server.js
+++ src/server.js
@@ -1,17 +1,24 @@
-import express, {json} from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
+import { categoriaRouter } from "./routes/categorias.routes.js";
 
 const app = express();
-app.use(json());
+// sirve para indicar a mi aplicacion de Express que puedo recibir la informacion en el body mediante un formato JSON
+// si no definimos nada, el body siempre llegara undefined porque no entendera que es lo que nos esta mandando el frontend
+// se suele llamar como Body Parser
+app.use(json()); // > Content-Type : application/json
 const PORT = process.env.PORT ?? 3000;
 
-app.listen(PORT, async() => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
-    try{
-        const respuesta = await mongoose.connect('mongodb://localhost:27817');
-        console.log("Se conecto a la base de datos   ");
-        console.log(respuesta);
-    }catch(error){
-        console.log(error);
-    }
+// definimos las rutas
+app.use(categoriaRouter);
+// fin de la definicion
+
+app.listen(PORT, async () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  try {
+    await mongoose.connect(process.env.MONGODB);
+    console.log("Se conecto a la base de datos 🔌");
+  } catch (error) {
+    console.log(error);
+  }
 });
