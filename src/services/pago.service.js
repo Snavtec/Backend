2aa2bceeb21f55288ce5664c45d2d2194421f986
Snavtec <samuel.navarro2305@gmail.com>
2022-02-25 22:19:59 -0500

    chore: modificacion de req.params a req.query

--- src/services/pago.service.js
+++ src/services/pago.service.js
@@ -3,46 +3,71 @@ import { Cliente } from "../models/cliente.model.js";
 import { Producto } from "../models/producto.model.js";
 
 export class PagoService {
-  static async generarPreferenciaDePago({ item, cliente }) {
+  static async generarPreferenciaDePago({ items, cliente }) {
     // https://www.mercadopago.com.pe/developers/es/reference/preferences/_checkout_preferences/post
     try {
       // buscar ese cliente
+      const clienteEncontrado = await Cliente.findById(cliente);
+      // buscar esos productos (items)
+      // hacer la busqueda de los productos usando el .map() (Usar el promise.all())
+      const productosEncontrados = await Promise.all(
+        items.map(async ({ id, cantidad }) => {
+          const productoEncontrado = await Producto.findById(id);
+
+          return { item: productoEncontrado, cantidad };
+        })
+      );
+
+      // early return function
+      const itemsMP = productosEncontrados.map(({ item, cantidad }) => ({
+        id: item._id,
+        title: item.nombre,
+        quantity: cantidad,
+        unit_price: +item.precio,
+        currency_id: "PEN",
+      }));
+
       const preferencia = await mercadopago.preferences.create({
-        // buscar esos productos (items)
-        // hacer la busqueda de los productos usando el .map() 
         payer: {
-          name: "Samuel", // obligatorio
-          surname: "Navarro", // obligatorio
+          name: clienteEncontrado.nombre, // obligatorio
+          surname: clienteEncontrado.apellido, // obligatorio
           address: {
-            zip_code: "04002",
-            street_name: "Calle Los Platanitos",
-            street_number: 123,
+            zip_code: clienteEncontrado.direccion.zip,
+            street_name: clienteEncontrado.direccion.calle,
+            street_number: clienteEncontrado.direccion.numero,
           },
-          email: "test_user_46542185@testuser.com",
-          identification: {
-            type: "DNI",
-            number: "45678945",
-          },
+          email: clienteEncontrado.correo,
         },
-        items: [
-          {
-            id: "1234",
-            title: "zapatillas de running",
-            quantity: 1,
-            unit_price: 115.0,
-            // https://es.wikipedia.org/wiki/ISO_4217
-            currency_id: "PEN",
-          },
-        ],
+        payment_methods: {
+          // https://api.mercadopago.com/v1/payment_methods
+          default_installments: 2, // el numero de cuotas por defecto que aparecera en el formulario
+          installments: 3, // maximo numero de cuotas que puede sacar un usuuario con tarjeta de credito
+          excluded_payment_methods: [
+            {
+              id: "diners",
+            },
+            {
+              id: "debvisa",
+            },
+          ],
+          excluded_payment_types: [
+            // debit_card credit_card atm
+            {
+              id: "atm",
+            },
+          ],
+        },
+        items: itemsMP,
         // Si la transaccion fue exitosa o pendiente de pago entonces nos redireccionara automaticamente a la pagina en cuestion
         auto_return: "approved",
         // son las url que me llevaran al sitio si el pago fue :
         back_urls: {
-          success: "http://localhost:3000/exito",
-          pending: "http://localhost:3000/pendiente",
-          failure: "http://localhost:3000/fallo",
+          success: `${process.env.DOMINIO}/exito`,
+          pending: `${process.env.DOMINIO}/pendiente`,
+          failure: `${process.env.DOMINIO}/fallo`,
         },
-        notification_url: `${ process.env.DOMINIO }/notificaciones`,
+        // Sera donde mercadopago hara el uso de notificaciones para informarme sobre el estado del pago
+        notification_url: `${process.env.DOMINIO}/notificaciones`,
       });
 
       return {
@@ -54,5 +79,10 @@ export class PagoService {
         error: error.message,
       };
     }
+  }
+
+  static recibirNotificacion(data) {
+    console.log("la data es: ");
+    console.log(data);
   }
 }
